36413d5a680e7d1aa9ea32bf405c491fc8fd236d
Anup Sunar <anupsunar09@gmail.com>
2023-05-04 08:02:22 +0000

    Updating Api and GUI Junit test

---
+++ WatherApi3Test.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class WatherApi3Test {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+	    // Test method
+	    // Add your test logic here
+	    // Use assertions to verify the expected behavior
+
+	    // Example test logic and assertions
+	    int expectedResult = 42;
+	    int actualResult = 20 + 22;
+	    assertEquals(expectedResult, actualResult);
+	    // Add more assertions based on your requirements
+	}
+}
